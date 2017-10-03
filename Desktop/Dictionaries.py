1198b236d1ad5f584acf29af8b7012010b8fd2ac
manycc <32350323+manycc@users.noreply.github.com>
2017-10-03 14:53:19 +1100

    Update Dictionaries.py

--- Desktop/Dictionaries.py
+++ Desktop/Dictionaries.py
@@ -1,14 +1,19 @@
 # import various text constants from `constants`
 from constants import *
+'''
+Import quotes used in the function below
+'''
 
 def freq_threshold(text, n):
+    # break the text into individual characters and count the occurence of each
     char_dict = {}
     for char in text:
         if char in char_dict:
             char_dict[char] += 1
         else:
             char_dict[char] = 1
-
+    
+    # calculate the characters which occur more than the user defined int n
     total = 0
     for count in char_dict.values():
         if count > n:
